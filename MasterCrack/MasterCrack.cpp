0fac74c82e1d3e64486af2aa8095f75852466291
bear102 <58755251+bear102@users.noreply.github.com>
2023-05-14 11:47:24 -0500

    Add files via upload

---
+++ MasterCrack/MasterCrack.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <vector>
+#include <cmath>
+
+int MasterCrack() {
+    std::vector<int> second;
+    std::vector<int> third;
+
+    int firstLockedPosition = 4;
+    int secondLockedPosition = 7;
+    int resistantLocation = 23;
+    int looseThird = 4;
+
+    // Calculates the first number
+    int first = static_cast<int>(std::ceil(resistantLocation) + 5) % 40;
+    int mod = first % 4;
+
+    // Calculates the third numbers
+    for (int i = 0; i < 4; i++) {
+        if (((10 * i) + firstLockedPosition) % 4 == mod) {
+            third.push_back((10 * i) + firstLockedPosition);
+        }
+
+        if (((10 * i) + secondLockedPosition) % 4 == mod) {
+            third.push_back((10 * i) + secondLockedPosition);
+        }
+    }
+
+    // Calculates the second numbers
+    int x;
+    if (looseThird == third[0]) {
+        x = 1;
+    } else {
+        x = 2;
+    }
+
+    for (int i = 0; i < 10; i++) {
+        int tmp = ((mod + 2) % 4) + (4 * i);
+        if (x == 0 || ((third[x - 1] + 2) % 40 != tmp && (third[x - 1] - 2) % 40 != tmp)) {
+            second.push_back(tmp);
+        }
+    }
+
+    std::cout << "First: " << first << std::endl;
+    std::cout << "Second: ";
+    for (int num : second) {
+        std::cout << num << " ";
+    }
+    std::cout << std::endl;
+    std::cout << "Third: " << looseThird << std::endl;
+
+
+    return 0;
+}
+
+int main()
+{
+    MasterCrack();
+    return 0;
+}
---
+++ MasterCrack/MasterCrack.js
@@ -0,0 +1,36 @@
+let second = [];
+let third = [];
+
+let first_locked_position = 4; // change
+let second_locked_position = 7; // change
+let resistant_location = 23; // change
+let loose_third = 4; // change
+
+// calculates first number
+let first = (Math.ceil(resistant_location) + 5) % 40;
+let mod = first % 4;
+
+// calculates third number
+for (let i = 0; i < 4; i++) {
+  if (((10 * i) + first_locked_position) % 4 === mod) {
+    third.push((10 * i) + first_locked_position);
+  }
+
+  if (((10 * i) + second_locked_position) % 4 === mod) {
+    third.push((10 * i) + second_locked_position);
+  }
+}
+
+// calculates second numbers
+let x = (loose_third === third[0]) ? 1 : 2;
+
+for (let i = 0; i < 10; i++) {
+  let tmp = ((mod + 2) % 4) + (4 * i);
+  if (!x || ((third[x-1] + 2) % 40 !== tmp && (third[x-1] - 2) % 40 !== tmp)) {
+    second.push(tmp);
+  }
+}
+
+console.log("First:", first);
+console.log("Second:", second);
+console.log("Third:", loose_third);
